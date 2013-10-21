7a392364b5b3886ad4f1d2ef422d0d4aa2a28190
galenwilk <galen@leaf.me>
2013-10-20 20:54:31 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nhl94::Application.config.secret_key_base = 'e588d063abaa225cb54dcfd73c595d15aacf3b35b1f4c28feb08dfe1c3a04638ce6ba139cd5bde50262deb7d33a7af2d8f0635df74a6b923e5796d869393701d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nhl94::Application.config.session_store :cookie_store, key: '_nhl94_session'
