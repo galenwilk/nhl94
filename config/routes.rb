c0121ff6f466af0441571aa5d3249abac26e5f86
galenwilk <galen@leaf.me>
2013-10-26 19:26:23 -0400

    add teams db, seed and CRUD.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Nhl94::Application.routes.draw do
 
-
-	
   devise_for :users
 	root :to => "home#index"
 	
 	resources :results
+	resources :teams
 	
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131026213058) do
+ActiveRecord::Schema.define(version: 20131026224813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20131026213058) do
     t.integer  "team2Score"
     t.integer  "player1Id"
     t.integer  "player2Id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "teams", force: true do |t|
+    t.string   "name"
+    t.integer  "rating"
+    t.string   "logo"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
