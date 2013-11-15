1545b5a649dec900898c01bb747ad29c63722ac8
Matt Barackman <mbarackman@leaf.me>
2013-11-14 19:27:17 -0500

    Set up results associations.

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,8 +1,12 @@
 class Result < ActiveRecord::Base
 
   attr_accessible :date, :player1Id, :team1, :team1Score, :player2Id, :team2, :team2Score
-  
-  has_many :players
-  has_many :teams
-  
+
+  def teams
+    Team.where{teams.id.eq(my{team1}) | teams.id.eq(my{team2})}
+  end
+
+  def players
+    Player.where{players.id.eq(my{player1Id}) | players.id.eq(my{player2Id})}
+  end
 end
